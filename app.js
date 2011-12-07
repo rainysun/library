14ee1068c976e2b8ec1497012939fe5b81358775
Rainy <aplysia@126.com>
2011-12-08 00:18:28 +0800

    (index & adm/login)'s views & router

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
 
 var app = module.exports = express.createServer();
 
@@ -17,6 +16,9 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+//session & cookie
+  app.use(express.cookieParser());
+  app.use(express.session({secret: 'rainy'}));
 });
 
 app.configure('development', function(){
@@ -29,7 +31,8 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+require('./routes/index')(app);
+require('./routes/adm')(app);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
