e03ed9e393e4230ec400be25b6fd4c21637d9293
Rainy <rainy.sun@fu101.com>
2011-12-13 18:03:24 +0800

    homepage css & empty bug:(

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ var search = require('../models/search');
 
 module.exports = function(app){
     app.get('/', function(req, res){
-	res.render('index', {title: 'Library', layout: 'layout'});
+	res.render('index', {title: 'Virtual Library', layout: 'layout'});
     });
     app.post('/base_search', function(req, res){
 	var sb = req.body;
 	var sel = {};
+	var empty = true;
 	for (i in sb){
-	    if (sb[i] != ''){
 		sel[i] = sb[i];
 	    }
 	}
